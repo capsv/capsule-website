3792a3a3dfefd96360246f2f2155b44cd30a8c0b
trapper <trapp.umin@gmail.com>
2024-05-22 16:35:27 +0300

    fear(header/sign-up): add header and sign up components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,28 @@
-// src/components/Header.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/Header.css';
+import SignUpModal from './SignUpModal';
 
 function Header() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <header className="header-container">
             <div className="logo">Capsule</div>
             <nav className="nav">
                 <button className="button">Sign In</button>
-                <button className="button">Sign Up</button>
+                <button className="button" onClick={openModal}>Sign Up</button>
             </nav>
+            <SignUpModal isOpen={isModalOpen} onClose={closeModal} />
         </header>
     );
 }
 
 export default Header;
-
