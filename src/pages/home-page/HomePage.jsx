d42f30f7076991a190260e977db4f73c1f7042cd
trapper <trapp.umin@gmail.com>
2024-06-16 17:30:29 +0300

    feat(HomePage): created headers for cards

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -36,14 +36,19 @@ function HomePage() {
                 <h2>{content[language].relevance}</h2>
                 <div className="relevance-cards">
                     <div ref={refCard1} className={`relevance-card ${inViewCard1 ? 'fade-in-left' : ''}`}>
+                        <h3>Word stat</h3>
                         <p>{content[language].relevanceDescription1}</p>
+                        <img src="/photos/Anxiety-rafiki.svg" alt="Anxiety" className="relevance-image-svg" />
                     </div>
                     <div ref={refCard2} className={`relevance-card ${inViewCard2 ? 'fade-in-right' : ''}`}>
+                        <h3>Word stat</h3>
                         <p>{content[language].relevanceDescription2}</p>
+                        <img src="/photos/Social anxiety-rafiki.svg" alt="Social Anxiety" className="relevance-image-svg" />
                     </div>
                 </div>
                 <div ref={refImage} className={`relevance-image-container ${inViewImage ? 'fade-in-up' : ''}`}>
-                    <img src="../../../public/photos/chart.png" alt="Relevance" className="relevance-image" />
+                    <h3>Word stat</h3>
+                    <img src="/photos/chart.png" alt="Relevance" className="relevance-image"/>
                 </div>
             </section>
         </div>
