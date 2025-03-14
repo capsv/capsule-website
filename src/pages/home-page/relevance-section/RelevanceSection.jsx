91f5b3a46c5cd1298e581328c4faa82c4bcb6dfd
trapper <trapp.umin@gmail.com>
2025-03-14 20:58:23 +0300

    k -> M

--- src/pages/home-page/relevance-section/RelevanceSection.jsx
+++ src/pages/home-page/relevance-section/RelevanceSection.jsx
@@ -215,8 +215,10 @@ const RelevanceSection = ({ content, lang }) => {
             family: "'Montserrat', sans-serif"
           },
           callback: function(value) {
-            if (value >= 1000) {
+            if (value >= 1000 && value < 1000000) {
               return (value / 1000) + 'k';
+            } else if (value >= 1000000) {
+              return (value / 1000000) + 'M';
             }
             return value;
           }
